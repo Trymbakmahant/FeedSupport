48f70e3bb1704bc93e79ee7787bb1679fe9967fe
Trymbak Mahant <trymbak0102mahant@gmail.com>
2024-08-02 23:22:18 +0530

    theme switcher added

---
+++ frontend/components/ThemeToggle.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React from "react";
+import { useState, useEffect } from "react";
+
+const ThemeToggle = () => {
+  const [darkMode, setDarkMode] = useState(true);
+
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+      setDarkMode(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkMode]);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setDarkMode((prev) => !prev);
+        }}
+      >
+        {" "}
+        {darkMode ? "dark" : "light"}
+      </button>
+    </div>
+  );
+};
+
+export default ThemeToggle;
